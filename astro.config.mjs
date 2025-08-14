5cb00b659203ca29da9fface728f8186ba98ce87
kabutoa <yinyun957@163.com>
2025-08-14 10:55:43 +0800

    feat: 完成官方教程

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://delightful-granita-42e804.netlify.app/",
+  integrations: [preact()],
 });
